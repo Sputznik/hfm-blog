a306368cafe107c3113eaf0e243b513ad18881e4
Steve <anil.stephen66@gmail.com>
2020-02-28 16:14:25 +0530

    Updated single-post template

--- single.php
+++ single.php
@@ -1,27 +1,53 @@
-<?php get_header();?>
-  <div class="container" style="margin-top: 80px;">
-    <div class="row">
-      <div class="col-md-8">
-        <?php if( have_posts() ): while( have_posts() ) : the_post();?>
-        <div class="entry-header text-center">
-          <h1 class="entry-title"><?php the_title();?></h1>
-        </div>
-        <div class="entry-content">
-          <p class="excerpt text-center"><em><?php echo excerpt(30);?></em></p>
-          <div class="main-content">
-            <?php the_content();?>
-          </div>
-        </div>
-        <div class="entry-meta">
-          <span class="tags">
-            Tagged: <?php the_tags( '', ', ', '' ); ?>
-          </span>
-        </div>
-        <?php endwhile;endif;?>
+<?php get_header(); $flag = 0;?>
+<div class="container" style="margin-top: 80px;">
+  <div class="row">
+    <div class="col-md-8"  style="padding-right: 20px;">
+      <?php if( have_posts() ): while( have_posts() ) : the_post();?>
+      <div class="entry-header text-center">
+        <h1 class="entry-title"><?php the_title();?></h1>
       </div>
-      <div class="col-md-4">
-        <?php echo do_shortcode( "[sidebar]" );?>
+      <div class="entry-content">
+        <p class="excerpt text-center"><em><?php echo excerpt(30);?></em></p>
+        <div class="article-decoration"></div>
+        <div class="main-content">
+          <?php the_content();?>
+        </div>
       </div>
+      <?php if( has_tag() ? $flag = 1 : $flag = 0 ):   ?>
+      <div class="entry-meta">
+        <span class="tags">
+          Tagged: <?php the_tags( '', ', ', '' );  ?>
+        </span>
+      </div>
+      <?php endif;?>
+      <?php endwhile;endif;?>
+      <div class="article-share">
+        <?php get_template_part( 'partials/share', 'socialmedia' );?>
+        <div class="clear"></div>
+      </div>
+      <div class="article-decoration-full"></div>
+      <nav class="article-pagination">
+        <span class="nav-previous pull-left"><strong><?php previous_post_link( '%link' ); ?></strong></span>
+        <span class="nav-next pull-right"><strong><?php next_post_link( '%link' ); ?></strong></span>
+      </nav><!-- #nav-single -->
+      <div class="clear" style="margin-bottom: 50px;"></div>
+    </div>
+    <div class="col-md-4">
+      <?php echo do_shortcode( "[sidebar]" );?>
     </div>
   </div>
+  <!-- Section display's posts having same post_tags -->
+  <?php if( $flag ): ?>
+  <?php $html = do_shortcode( '[orbit_related_query taxonomy="post_tag" style="grid3" posts_per_page="-1"]' ); ?>
+    <?php if( strlen( $html ) ):?>
+    <div class="suggested-articles">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="title">Suggested Reading</h3>
+          <?php echo $html;?>
+        </div>
+      </div>
+    </div>
+  <?php endif;endif;?>
+</div>
 <?php get_footer();?>

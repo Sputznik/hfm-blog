02af57cb2321f57facf6854c51393c274b91ea97
Steve <anil.stephen66@gmail.com>
2020-03-03 12:04:47 +0530

    Updated styles

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 add_action('wp_enqueue_scripts',function(){
-  wp_enqueue_style('hfm-blog-style', get_stylesheet_directory_uri().'/assets/css/hfm-blog.css', array('sp-core-style'), time() ); //'1.0.8'
+  wp_enqueue_style('hfm-blog-style', get_stylesheet_directory_uri().'/assets/css/hfm-blog.css', array('sp-core-style'), '1.0.0' );
 });
 
 //Sidebar widget for navigation
